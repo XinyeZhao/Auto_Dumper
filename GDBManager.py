d89c7dda57c273a02ea61e222744196de10a0f52
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-12-06 14:44:21 -0500

    wrapper class

--- GDBManager.py
+++ GDBManager.py
@@ -1,8 +1,25 @@
-import pygdbmi
+from pygdbmi import gdbcontroller
 
 class GDBManager:
     '''
-    This class is designed to control GDB, which will be used to insert and/or remove breakpoint,
+    This class is designed to control GDB, which will be used to insert and/or remove breakpoint.
+    It is a wrapper around pygdbmi.gdbcontroller
     '''
     def __init__(self):
-        self.controller = pygdbmi.gdbcontroller
+        self.__controller = gdbcontroller.GdbController()
+        self.__isActive = True
+
+    def start():
+        if not self.__isActive:
+            self.__init__()
+
+    def exec(self, command):
+        output = self.__controller.write(command)
+        return output
+
+    def exit(self):
+        if self.__isActive:
+            self.__controller.exit()
+            self.__isActive = False
+
+        
--- auto_dumper.py
+++ auto_dumper.py
@@ -4,7 +4,8 @@ import GDBManager
 def main():
     process_manager = ProcessManager()
     process_manager.showAllProcInfo()
-    process_manager.processKiller('wallpaper32.exe')
+    
+    
 
 if __name__ == '__main__':
     main()
