2be0b8f005abc628951327a851b2660cfd8624c9
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-12-06 15:49:54 -0500

    remove process management temporarily

--- auto_dumper.py
+++ auto_dumper.py
@@ -1,4 +1,3 @@
-from ProcessManager import ProcessManager
 from GDBManager import GDBManager
 import sys
 
@@ -6,9 +5,6 @@ def prompt():
     print("Usage: python auto_dumper.py [breakpoint address in hex]")
 
 def main():
-    process_manager = ProcessManager()
-    process_manager.showAllProcInfo()
-    
     if(len(sys.argv) != 2):
         prompt()
         return
@@ -19,23 +15,21 @@ def main():
     except:
         prompt()
         return
-
+    
     print("Launching gdb subprocess...")
     gdb_manager = GDBManager()
     print("gdb launched successfully.")
 
     # add breakpoint
     print("Adding break point...")
-    gdb_manager.exec(f'break {addr}')
+    output = gdb_manager.exec(f'break {addr}')
     print(f"Response for adding breakpoint at {addr}:")
-    output = gdb_manager.get_response()
     print(output)
 
     # remove breakpoint
     print("Removing break point...")
-    gdb_manager.exec(f'del 1')
+    output = gdb_manager.exec(f'del 1')
     print(f"Response for adding breakpoint at {addr}:")
-    output = gdb_manager.get_response()
     print(output)
 
     gdb_manager.exit()
