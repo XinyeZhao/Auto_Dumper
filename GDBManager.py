460ffe2e25f38dad2a573588c8d0058e1253cd72
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-12-06 14:33:40 -0500

    add .gitignore

--- GDBManager.py
+++ GDBManager.py
@@ -5,4 +5,4 @@ class GDBManager:
     This class is designed to control GDB, which will be used to insert and/or remove breakpoint,
     '''
     def __init__(self):
-        pass
+        self.controller = pygdbmi.gdbcontroller
